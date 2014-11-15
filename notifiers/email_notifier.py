b77911bf1dc55488d4060a1dab487c96f94553cd
Anton <git@maestr0.com>
2014-11-15 16:17:21 +0100

    Add OSX notifier

--- notifiers/email_notifier.py
+++ notifiers/email_notifier.py
@@ -4,10 +4,8 @@ import logging
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from base_notifier import Notifier
+from notifiers.base_notifier import Notifier
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 _logger = logging.getLogger(__name__)
 
 
@@ -16,17 +14,16 @@ class EmailNotifier(Notifier):
 
     def __init__(self, config):
         """Override init to make SMTP-settings check"""
-        super(EmailNotifier, self).__init__(config)
-        self.fromaddr = self.config['from_email']
+        self.fromaddr = config['from_email']
         # smtp user may be different from email
-        self.fromuser = self.config.get('from_user', self.fromaddr)
-        self.frompwd = self.config['from_pwd']
-        self.host = self.config['from_smtp_host']
-        self.port = self.config['from_smtp_port']
-        self.toaddr = self.config['to_email']
-        self.check_smtp_server()
+        self.fromuser = config.get('from_user', self.fromaddr)
+        self.frompwd = config['from_pwd']
+        self.host = config['from_smtp_host']
+        self.port = config['from_smtp_port']
+        self.toaddr = config['to_email']
+        super(EmailNotifier, self).__init__(config)
 
-    def check_smtp_server(self):
+    def check_requirements(self):
         """Logs in to SMTP server to check credentials and settings"""
         server = smtplib.SMTP(self.host, self.port)
         server.ehlo()
