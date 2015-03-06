3518d2d33a4126d5440b984f56cb6ecd57f4576f
Anton <git@maestr0.com>
2015-03-06 15:44:41 +0100

    Some refactoring and better logging

--- crawler.py
+++ crawler.py
@@ -56,13 +56,16 @@ STATES = {}
 
 def update_state(state, value, message=False):
     """Update state of particular event"""
-    # if new value is True and last saved was False
+    # if state is new, init it as False
     if state not in STATES:
         STATES[state] = False
+    # compare new value to old value
     if value is not STATES[state]:
         _logger.info("State change - %s: %s", state, value)
+    # notify, if state changed from False to True
     if value and not STATES[state]:
-        notifier.notify(**message)
+        NOTIFIER.notify(**message)
+    # save the new value
     STATES[state] = value
 
 
@@ -73,66 +76,68 @@ def run_crawler():
     # request OVH availablility API asynchronously
     try:
         response = yield http_client.fetch(URL)
-    except HTTPError as e:
-        _logger.info("HTTP Error: {0}".format(e))
+    except HTTPError as ex:
+        _logger.error("HTTP Error: {0}".format(ex))
         return
     response_json = json.loads(response.body.decode('utf-8'))
     if not response_json or not response_json['answer']:
         return
     availability = response_json['answer']['availability']
     for item in availability:
-        # look for servers of required types in OVH availability list
-        if SERVER_TYPES.get(item['reference']) in config['servers']:
-            # make a flat list of zones where servers are available
-            available_zones = [e['zone'] for e in item['zones']
-                               if e['availability'] not in ['unavailable',
-                                                            'unknown']]
-            # iterate over all tacked zones and set availability states
-            for zone in config['zones']:
-                server = SERVER_TYPES[item['reference']]
-                state_id = '%s_available_in_%s' % (server, zone)
-                # update state for each tracked zone
-                text = "Server %s is available in %s" % (server, zone)
-                message = {
-                    'title': "Server %s available" % server,
-                    'text': text,
-                    'url': "http://www.kimsufi.com/fr/index.xml"
-                }
-                update_state(state_id, zone in available_zones, message)
+        # get server type of availability item
+        server_type = SERVER_TYPES.get(item['reference'])
+        # return if this server type is not tracked
+        if server_type not in CONFIG['servers']:
+            return
+        # make a flat list of zones where servers of this type are available
+        available_zones = [
+            e['zone'] for e in item['zones']
+            if e['availability'] not in ['unavailable', 'unknown']]
+        _logger.debug('%s is available in %s', server_type, available_zones)
+        # iterate over all tracked zones and update availability state
+        for zone in CONFIG['zones']:
+            server_available = zone in available_zones
+            state_id = '%s_available_in_%s' % (server_type, zone)
+            message = {
+                'title': "Server {0} available".format(server_type),
+                'text': "Server {server_type} is available in {zone}".format(
+                    server_type=server_type, zone=zone),
+                'url': "http://www.kimsufi.com/fr/index.xml"
+            }
+            update_state(state_id, server_available, message)
 
 
 if __name__ == "__main__":
     CONFIG_NAME = sys.argv[1] if len(sys.argv) == 2 else 'config.json'
     with open(CONFIG_NAME, 'r') as configfile:
         try:
-            config = json.loads(configfile.read())
+            CONFIG = json.loads(configfile.read())
         except ValueError:
             _logger.error("Parsing JSON settings in config.json has failed. "
                           "Check syntax with a validator (i.e. jsonlint.com)")
             sys.exit(1)
 
-
     # Select notifier, 'email' by default
-    if 'notifier' not in config:
+    if 'notifier' not in CONFIG:
         _logger.warning("No notifier selected in config, 'email' will be used")
-        config['notifier'] = 'email'
+        CONFIG['notifier'] = 'email'
     # Instantiate notifier class dynamically
     try:
-        n_path = NOTIFIERS[config['notifier']]
-        n_file, n_classname = n_path.rsplit('.', 1)
-        n_module = importlib.import_module(n_file)
-        notifier = getattr(n_module, n_classname)(config)
-    except:
+        NOTIFIER_PATH = NOTIFIERS[CONFIG['notifier']]
+        NOTIFIER_FILE, NOTIFIER_CLASSNAME = NOTIFIER_PATH.rsplit('.', 1)
+        NOTIFIER_MODULE = importlib.import_module(NOTIFIER_FILE)
+        NOTIFIER = getattr(NOTIFIER_MODULE, NOTIFIER_CLASSNAME)(CONFIG)
+    except Exception as ex:
         _logger.exception("Notifier loading failed,"
                           "check configuration for errors")
         sys.exit(1)
 
-    loop = tornado.ioloop.IOLoop.instance()
-    tornado.ioloop.PeriodicCallback(run_crawler, 30000).start()
+    LOOP = tornado.ioloop.IOLoop.instance()
+    tornado.ioloop.PeriodicCallback(run_crawler, 15000).start()
     _logger.info("Starting IO loop")
 
     try:
-        loop.start()
+        LOOP.start()
     except KeyboardInterrupt:
         _logger.info("Terminated by user. Bye.")
         sys.exit(0)
