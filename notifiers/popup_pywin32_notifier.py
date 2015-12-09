2878a49a70cf90e92a26694384098e8323fd9491
Alexander Price <nullprice@gmail.com>
2015-12-09 03:06:09 +0000

    Added in new Notifier object for PyWin32 specific message box.

---
+++ notifiers/popup_pywin32_notifier.py
@@ -0,0 +1,32 @@
+"""Notifier that opens a popup window"""
+
+import logging
+import webbrowser
+from notifiers.base_notifier import Notifier
+
+_logger = logging.getLogger(__name__)
+
+
+class PopupPywin32Notifier(Notifier):
+    """Notifier class for popup windows, a windows specific version"""
+
+    def check_requirements(self):
+        """Check platform and notifier command"""
+        lib_name = "win32api"
+        try:
+            __import__(lib_name)
+        except ImportError:
+            raise Warning(
+                "{} Python library is required".format(lib_name))
+
+    def notify(self, title, text, url=False):
+        """Open popup notification window with easygui"""
+        import win32api
+        # 0x00001000 -- Value represents MB_SYSTEMMODAL
+        # This is to allow for the messagebox to sit over every window
+        # Something that is not possible using easygui (as far as I'm aware)
+        if win32api.MessageBox(0,
+                               'Open Kimsufi order form?',
+                               'Kimsufi Crawler',
+                               0x00001000) == 1:
+            webbrowser.open_new_tab(url)
